3ea91b0a644b7fa18038375da4029007b8de6684
jio <jio.chenhao@gmail.com>
2026-04-21 11:39:50 +0800

    修复BUG

--- Steam/Errors/error.go
+++ Steam/Errors/error.go
@@ -34,8 +34,12 @@ func printStack(errKey, errStr string) error {
 	message := fmt.Sprintf(errKey, errStr)
 	message += "\n" + string(buf[:n]) // 添加堆栈信息
 
-	// return fmt.Errorf(errKey, message)
-	return errors.New(errKey)
+	if len(message) > 20 {
+		message = message[:20]
+	}
+
+	return fmt.Errorf(errKey, message)
+	// return errors.New(errKey)
 }
 
 // Error 创建通用错误
